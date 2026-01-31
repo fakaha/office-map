98026ab59524bf5cecd80592234d110749c30d17
fakaha <zulfakaha@students.amikom.ac.id>
2026-02-01 01:39:24 +0700

    fix

--- routes/web.php
+++ routes/web.php
@@ -12,11 +12,11 @@ Route::get('posts', [PostController::class, 'index']);
 Route::middleware('auth')->group(function () {
     Route::post('posts', [PostController::class, 'store'])->name('posts.store');
     Route::get('posts/create', [PostController::class, 'create'])->middleware('auth');
+    Route::delete('post/{id}/delete', [PostController::class, 'destroy']);
+    Route::get('post/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
+    Route::put('post/{id}/update', [PostController::class, 'update'])->name('posts.update');
 });
 
-Route::delete('post/{id}/delete', [PostController::class, 'destroy']);
-Route::get('post/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
-Route::put('post/{id}/update', [PostController::class, 'update'])->name('posts.update');
 Route::get('post/{id}', [PostController::class, 'show'])->name('posts.show');
 
 Route::middleware(['auth', 'verified'])->group(function () {
